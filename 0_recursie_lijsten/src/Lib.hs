c7ecefdb8ec2c42f27146c1658af9bb351da74e2
AdamMC-GL <mahmoudchebil@live.nl>
2021-01-29 14:47:49 +0100

    Update Lib.hs

--- 0_recursie_lijsten/src/Lib.hs
+++ 0_recursie_lijsten/src/Lib.hs
@@ -2,37 +2,38 @@ module Lib
     ( ex1, ex2, ex3, ex4, ex5, ex6, ex7
     ) where
 
--- Schrijf een functie die de som van een lijst getallen berekent, net als de `product` functie uit de les.
+
+-- | Een functie die de som van een lijst berekent
 ex1 :: [Int] -> Int
-ex1 [a] = a
-ex1 a = (head a) + ex1 (tail a)
+ex1 [a] = a --base case, als de lijst 1 getal bevat wordt dat getal als int teruggegeven
+ex1 a = (head a) + ex1 (tail a) --head van de lijst optellen met ex1 met als input de tail, de uikomst is de head van die tail
 
--- Schrijf een functie die alle elementen van een lijst met 1 ophoogt; bijvoorbeeld [1,2,3] -> [2,3,4]. Het is voor deze opgave nog niet de bedoeling dat je hogere-orde functies gebruikt.
+-- | Een functie die de elk element in de lijst verhoogt met 1. [1,2,3] -> [2,3,4]
 ex2 :: [Int] -> [Int]
-ex2 [a] = [a]
-ex2 a = map (1+) a
+ex2 [a] = [a+1]
+ex2 a = head a+1 : (ex2 (tail a))
 
--- Schrijf een functie die alle elementen van een lijst met -1 vermenigvuldigt; bijvoorbeeld [1,-2,3] -> [-1,2,-3]. Het is voor deze opgave nog niet de bedoeling dat je hogere-orde functies gebruikt.
+-- | Een functie die elk element in de lijst vermenigvuldigt met -1, dus negatief naar positief maakt of visa versa. [1,-2,3] -> [-1,2,-3]
 ex3 :: [Int] -> [Int]
-ex3 [a] = [a]
-ex3 a = map ((-1)*) a
+ex3 [a] = [a*(-1)]
+ex3 a = head a*(-1) : (ex3 (tail a))
 
--- Schrijf een functie die twee lijsten aan elkaar plakt, dus bijvoorbeeld [1,2,3] en [4,5,6] combineert tot [1,2,3,4,5,6]. Maak hierbij geen gebruik van de standaard-functies, maar los het probleem zelf met (expliciete) recursie op. Hint: je hoeft maar door een van beide lijsten heen te lopen met recursie.
+-- | Een functie die 2 lijsten met elkaar concateneert. [1,2,3] [4,5,6] -> [1,2,3,4,5,6]
 ex4 :: [Int] -> [Int] -> [Int]
-ex4 [a] [b] = [a]
-ex4 a b = a ++ b
+ex4 [] b = b
+ex4 a b = head a : ex4 (tail a) (b)
 
--- Schrijf een functie die twee lijsten paarsgewijs bij elkaar optelt, dus bijvoorbeeld [1,2,3] en [4,5,6] combineert tot [1+4, 2+5, 3+6] oftewel [5,7,9]. Het is voor deze opgave nog niet de bedoeling dat je hogere-orde functies gebruikt.
+-- | Een functie die de elementen plekgewijs met elkaar optelt met een lijst als uitkomst. [1,2,3] [4,5,6] -> [5,7,9]
 ex5 :: [Int] -> [Int] -> [Int]
-ex5 [a] [b] = [a]
-ex5 a b = zipWith (+) a b
+ex5 [] [] = []
+ex5 a b = (head a) + (head b) : (ex5 (tail a) (tail b))
 
--- Schrijf een functie die twee lijsten paarsgewijs met elkaar vermenigvuldigt, dus bijvoorbeeld [1,2,3] en [4,5,6] combineert tot [1*4, 2*5, 3*6] oftewel [4,10,18]. Het is voor deze opgave nog niet de bedoeling dat je hogere-orde functies gebruikt.
+-- | Een functie die de elementen plekgewijs met elkaar vermenigvuldigt met een lijst als uitkomst. [1,2,3] [4,5,6] -> [4,10,18]
 ex6 :: [Int] -> [Int] -> [Int]
-ex6 [a] [b] = [a]
-ex6 a b = zipWith (*) a b
+ex6 [] [] = []
+ex6 a b = (head a) * (head b) : (ex6 (tail a) (tail b))
 
--- Schrijf een functie die de functies uit opgave 1 en 6 combineert tot een functie die het inwendig product uitrekent. Bijvoorbeeld: `ex7 [1,2,3] [4,5,6]` -> 1*4 + 2*5 + 3*6 = 32.
+-- | Een functie dat het inwendig product van 2 lijsten berekent. [1,2,3] [4,5,6] = 1*4 + 2*5 + 3*6 = 32.
 ex7 :: [Int] -> [Int] -> Int
 ex7 [a] [b] = a
 ex7 a b = ex1 (ex6 a b)
